ab43bd34bb85c5594468d5f2b8f5bb64d93ec977
604941884@qq.com <604941884@qq.com>
2019-01-12 22:54:07 +0800

    new file:   .idea/vcs.xml 	modified:   .idea/workspace.xml 	modified:   app01/__pycache__/views.cpython-36.pyc 	modified:   app01/views.py 	new file:   templates/detial.html 	new file:   templates/home.html 	new file:   templates/index.html 	modified:   templates/login.html 	modified:   untitled2/__pycache__/urls.cpython-36.pyc 	modified:   untitled2/urls.py 	new file:   upload/1.jpg

--- app01/views.py
+++ app01/views.py
@@ -1,22 +1,83 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.shortcuts import redirect
+from django.views import View
 
 # Create your views here.
 
+USER_DICT = {
+    '1':{'user':'root1','email':'root@live.com'},
+    '2':{'user':'root2','email':'root@live.com'},
+    '3':{'user':'root3','email':'root@live.com'},
+    '4':{'user':'root4','email':'root@live.com'},
+}
+
+# def detial(request):
+#
+#     nid = request.GET.get('nid')
+#     detial_info = USER_DICT[nid]
+#     return render(request,'detial.html',{'detial_info':detial_info})
+def detial(request,nid):
+    detial_info = USER_DICT[nid]
+    return render(request,'detial.html',{'detial_info':detial_info})
 
 def index(request):
-    return HttpResponse('index')
+    return render(request,'index.html',{'user_dict':USER_DICT})
+
+# def login(request):
+#     if request.method == "GET":
+#         return render(request, 'login.html')
+#     elif request.method == "POST":
+#         u = request.POST.get('user')
+#         p = request.POST.get('pwd')
+#         if u == 'alex' and p == '123':
+#             return redirect('/index/')
+#         else:
+#             return render(request,'login.html')
+#     else:
+#         return redirect('/index/')
 
 def login(request):
     if request.method == "GET":
         return render(request, 'login.html')
     elif request.method == "POST":
-        u = request.POST.get('user')
-        p = request.POST.get('pwd')
-        if u == 'alex' and p == '123':
-            return redirect('/index/')
-        else:
-            return render(request,'login.html')
+        # v = request.POST.get('gender')
+        # print(v)
+        # v = request.POST.getlist('favor')
+        # print(v)
+        # v2 = request.POST.getlist('city')
+        # print(v2)
+        # v = request.POST.get('fawenjian')
+        # print(v)
+        v2 = request.FILES.get('fawenjian')
+        print(v2,type(v2),v2.name)
+        import os
+        file_path=os.path.join('upload',v2.name)
+        f = open(file_path,mode='wb')
+        for i in v2.chunks():
+            f.write(i)
+        f.close()
+        return render(request,'login.html')
     else:
         return redirect('/index/')
+
+
+class Home(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        #调用父类中的dispatch
+        print('before')
+        result = super(Home,self).dispatch(request,*args,**kwargs)
+        print('after')
+        return result
+
+    def get(self,request):
+        print(request.method)
+        return render(request,'home.html')
+
+    def post(self,request):
+        print(request.method,'POST')
+        return render(request,'home.html')
+
+
+
--- untitled2/urls.py
+++ untitled2/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from app01 import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
     path('login/', views.login),
+    re_path('detial-(\d+).html', views.detial),
+    path('home/', views.Home.as_view()),
 ]
